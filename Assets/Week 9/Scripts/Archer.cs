1f91939c735664dfe8d64948e6a2baad00f5db10
Euliya <zhuyihongmaggie@gmail.com>
2024-03-21 14:08:38 -0400

    Task1 done.

--- Assets/Week 9/Scripts/Archer.cs
+++ Assets/Week 9/Scripts/Archer.cs
@@ -6,22 +6,16 @@ public class Archer : Villager
 {
     public GameObject arrowPrefab;
     public Transform spawnPoint;
-    public float delay=0.2f;
 
     protected override void Attack()
     {
         destination = transform.position;
         base.Attack();
-        Invoke("SpawnArrow",delay);
-    }
-
-    void SpawnArrow()
-    {
         Instantiate(arrowPrefab, spawnPoint.position, spawnPoint.rotation);
     }
+
     public override ChestType CanOpen()
     {
         return ChestType.Archer;
     }
-
 }
--- Assets/Week 9/Scripts/Thief.cs
+++ Assets/Week 9/Scripts/Thief.cs
@@ -7,18 +7,49 @@ public class Thief : Villager
     public GameObject knifePrefab;
     public Transform spawnPoint1;
     public Transform spawnPoint2;
+    //public float dashtime=2;
+    //float timer;
+    //bool isDashing;
+    float dashSpeed = 7;
+    Coroutine dashing;
 
-    protected override void Attack()
+    IEnumerator notDash()
+    {
+        yield return null;
+    }
+    IEnumerator Dash()
     {
         destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        speed = dashSpeed;
+        while (speed > 3) 
+        {
+            yield return null;
+        }
+
         base.Attack();
+        yield return new WaitForSeconds(0.1f);
         Instantiate(knifePrefab, spawnPoint1.position, spawnPoint1.rotation);
+        yield return new WaitForSeconds(0.2f);
         Instantiate(knifePrefab, spawnPoint2.position, spawnPoint2.rotation);
     }
 
+    
+    protected override void Attack()
+    {
+        if (dashing != null)
+        {
+            StopCoroutine(dashing);
+        }
+        dashing=StartCoroutine(Dash());
+    }
+    
+    
     public override ChestType CanOpen()
     {
         return ChestType.Thief;
     }
+    public override string ToString()
+    {
+        return "Hi, I am Feid!!";
+    }
 }
-
